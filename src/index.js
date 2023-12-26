88af3cf935aa79600eab40265c5c31a5d828ae4a
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-12-25 19:45:55 -0600

    new start on project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ScrollToTop } from './components';
 import App from './App';
 
-ReactDOM.render( <App />, document.getElementById('root') );
+const root = ReactDOM.createRoot( document.getElementById('root') );
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <ScrollToTop />
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
